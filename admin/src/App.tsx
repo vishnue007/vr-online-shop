2193741de89e6554b7b275885ce6e16ffa89e1f0
vishnue.bn <vishnue.bn@pitsolutions.com>
2026-04-05 12:47:36 +0530

    Initial commit - admin, frontend, backend

---
+++ admin/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Welcome to the VR Online Shop</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Welcome to the VR Online Shop</h1>
+    </div>
+  )
+}
+
+export default App
